1c31a0fc7e5ada12540d3ca7221b430c6566bf30
Jinho <jh8579@gmail.com>
2021-04-16 00:44:24 +0900

    refactor : spark를 사용해 application 실행할 수 있도록 변경

--- src/main/java/chess/SparkChessApplication.java
+++ src/main/java/chess/SparkChessApplication.java
@@ -1,22 +1,9 @@
 package chess;
 
-import static spark.Spark.port;
-import static spark.Spark.staticFiles;
-
-import chess.controller.SparkWebController;
-import chess.repository.GameDao;
-import chess.repository.GameHistoryDao;
-import chess.repository.UserDao;
-import chess.service.ChessService;
-import chess.service.UserService;
+import org.springframework.boot.SpringApplication;
 
 public class SparkChessApplication {
     public static void main(String[] args) {
-//        port(8080);
-//        staticFiles.location("/public");
-//        ChessService chessService = new ChessService(new GameDao(), new GameHistoryDao());
-//        UserService userService = new UserService(new UserDao());
-//        SparkWebController sparkWebController = new SparkWebController(chessService, userService);
-//        sparkWebController.run();
+        SpringApplication.run(SpringChessApplication.class, args);
     }
 }
